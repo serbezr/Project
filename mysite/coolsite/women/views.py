eefc640c72b74a6d4ebeca0b7d22ba0794672333
Sergey Bezrukov <aviastro47@mail.ru>
2022-06-12 10:22:39 +0300

    Обработка исключений 404

--- mysite/coolsite/women/views.py
+++ mysite/coolsite/women/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django http import HttpResponce, HttpResponseNotFound
 
 def index(request):
     return HttpResponce("Страница приложения women")
@@ -11,4 +13,9 @@ def categories(request,caidt):
 
 
 def archive(request,year):
+    if int(year)>2020:
+        raise Http 404
     return HttpResponce(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def pageNotFound(request,exeption):
+    return: HttpResponseNotFound(<h1>Страница не найдена</h1>)
