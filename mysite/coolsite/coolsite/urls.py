21854daa5da14faa0320e2b1edb3094cf08747e1
Sergey Bezrukov <aviastro47@mail.ru>
2022-06-12 08:01:56 +0300

    Добавляем еще один маршрут в coolsite/urls

--- mysite/coolsite/coolsite/urls.py
+++ mysite/coolsite/coolsite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from women.views import index
+from women.views import*
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('women/',index),
+    path('cats/',categories),
 ]
