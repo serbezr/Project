792eefe8dd4cc7307c2c459aa4f64804ad683d2a
Sergey Bezrukov <aviastro47@mail.ru>
2022-06-12 12:11:26 +0300

    Создание 301 и 302 редиректов

--- mysite/coolsite/women/views.py
+++ mysite/coolsite/women/views.py
@@ -1,15 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import redirect
 from django.shortcuts import render
-from django http import HttpResponce, HttpResponseNotFound
+from django http import HttpResponse, HttpResponseNotFound
 
 def index(request):
-    return HttpResponce("Страница приложения women")
+    return HttpResponse("Страница приложения women")
 
 
 def categories(request,caidt):
     if request.POST:
         print(request.POST)
-    return HttpResponce(f"<h1>Статьи по категориям</h><p>{catid}</p>")
+    return HttpResponse(f"<h1>Статьи по категориям</h><p>{catid}</p>")
 
 
 def archive(request,year):
@@ -19,3 +20,12 @@ def archive(request,year):
 
 def pageNotFound(request,exeption):
     return: HttpResponseNotFound(<h1>Страница не найдена</h1>)
+
+def archive(request,year):
+    if int(year)>2020:
+        return redirect('/')
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def archive(request,years):
+    if int(years) <2020:
+        return redirect('/', permanent=True)
