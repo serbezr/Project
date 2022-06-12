183ea913a4c1b558bc457b9013ed95058c779cd2
Sergey Bezrukov <aviastro47@mail.ru>
2022-06-12 09:25:14 +0300

    Формирование url адресов с slag

--- mysite/coolsite/women/views.py
+++ mysite/coolsite/women/views.py
@@ -4,5 +4,5 @@ def index(request):
     return HttpResponce("Страница приложения women")
 
 
-def categories(request,catid):
-    return HttpResponce(f"<h1>Статьи по категориям</h><p>{catid}</p>")
+def categories(request,cat):
+    return HttpResponce(f"<h1>Статьи по категориям</h><p>{cat}</p>")
